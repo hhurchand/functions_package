0e2fcb4bbf7ec08428025063e3d49375f3f9a89b
hhurchan <h.hurchand@gmail.com>
2023-08-18 17:12:12 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="is_number",
+    version="1.0.0",
+    author="H Hurchand",
+    author_email="h.hurchand@gmail.com",
+    description="A python module to work with functions",
+    long_description=long_description,
+    long_description_content_type="text/md",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10.8',
+    install_requires=requirements,
+)
