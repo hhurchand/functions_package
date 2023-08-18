e5bd38686cd6e063b2b2358a4beae09f2008b935
hhurchan <h.hurchand@gmail.com>
2023-08-18 16:48:59 -0400

    Add quadratic class and compute discriminant

--- src/quadratics.py
+++ src/quadratics.py
@@ -1,3 +1,14 @@
 class Quadratics:
-    def __init__(self):
-        pass
+    def __init__(self,coeff_x2: float,coeff_x:float,constant:float):
+        self.a = coeff_x2
+        self.b = coeff_x
+        self.c = constant
+    
+    def discriminant(self)-> float:
+        """Calculate discriminant of quadratic
+        
+        Returns:
+        Discrimant : Float
+
+        """
+        return self.b*self.b - 4*self.a*self.c
