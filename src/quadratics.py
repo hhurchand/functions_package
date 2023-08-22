4be55ac7f56209352023fc646b20b64b19b5c7b5
hhurchan <h.hurchand@gmail.com>
2023-08-22 08:49:23 -0400

    make flake8 compatible

--- src/quadratics.py
+++ src/quadratics.py
@@ -1,17 +1,33 @@
 class Quadratics:
-    def __init__(self,coeff_x2: float,coeff_x:float,constant:float):
+    """Calculate property indicators of a quadratic expression.
+
+    Given the coefficients of a quadratic expression, this class perform
+    key caulculations, which are indicative of properties of the quadratic
+    eg. discriminant, nature of roots
+
+
+    Attributes:
+    coeff_x2 (float): coeffiecient of x^2
+    coeff_x1 (float): coefficient of x
+    constant: constant term
+
+    Methods:
+    discriminant: Returns the discriminant of the quadratic expression
+    """
+    def __init__(self, coeff_x2: float, coeff_x: float, constant: float):
         self.a = coeff_x2
         self.b = coeff_x
         self.c = constant
-    
-    def __repr__(self):
-        return f"Class {self.__class__.__name__} calcuates discriminant of a quadratic"
 
-    def discriminant(self)-> float:
-        """Calculate discriminant of quadratic
-        
+    def __repr__(self):
+        return f"Class {self.__class__.__name__}\
+                    calcuates discriminant of a quadratic"
+
+    def discriminant(self) -> float:
+        """Calculate discriminant of quadratic.
+
         Returns:
-        Discriminant : Float
+        Discriminant (float): Calculates the discriminat of the quadratic
 
         """
         return self.b*self.b - 4*self.a*self.c
